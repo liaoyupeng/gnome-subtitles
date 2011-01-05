72f2a4cff9931b6443b970d9312a9903f5fccae6
Yupeng Liao <yupeng@hulu.com>
2011-01-05 16:35:57 +0800

    code style fix

--- src/SubLib/IO/SubtitleFormats/SubtitleFormatSAMI.cs
+++ src/SubLib/IO/SubtitleFormats/SubtitleFormatSAMI.cs
@@ -26,39 +26,37 @@ namespace SubLib.IO.SubtitleFormats {
 
 internal class SubtitleFormatSAMI : SubtitleFormat {
 
-	internal SubtitleFormatSAMI () {		
-		name = "SAMI";
-		type = SubtitleType.SAMI;
-		mode = SubtitleMode.Times;
-		extensions = new string[] { "smi" };
-		lineBreak = "|";
-		
-		format = @"\s*\<sami\>\s*([^<]|<(?!body>))*<body>";
-		headers=new string[2]{@"<SAMIParam[^>]*>([^<]|<(?!/SAMIParam>))*</SAMIParam>",@"<STYLE[^>]*>([^<]|<(?!/STYLE>))*</STYLE>" };
-		bodyEndIn = @"</BODY>\s*(</SAMI>)?"; //IGNORE CASE
-		bodyBeginOut = "<SAMI><HEAD></HEAD>\n<BODY>"; 
-		bodyEndOut = "</BODY>\n</SAMI>";
-		
-		subtitleIn = @"<SYNC\s*start=[^0-9>]*(?<StartMilliseconds>\d*)[^0-9>]*>(?<Text>([^<]|<(?!/?SYNC))*)(</SYNC\s*>)?\s*<SYNC\s*start=[^0-9>]*(?<EndMilliseconds>\d*)[^0-9>]*>([^<]|<(?!/?SYNC))*(</SYNC\s*>)?";
-		subtitleOut = "<SYNC Start=\"<<StartTotalMilliseconds>>\"><P class=\"ENUSCC\"><<Text>></P></SYNC>\n<SYNC Start=\"<<EndTotalMilliseconds>>\"><P class=\"ENUSCC\">&#160;</P></SYNC>";
-	}
-	
-	internal override void SubtitleInputPostProcess (Subtitle subtitle) {
-		Regex regPara = new Regex(@"\s*<P[^>]*>", RegexOptions.IgnoreCase);
-		Regex regParaClose = new Regex(@"</P\s*>\s*", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
-		string oriText = subtitle.Text.GetTrimLines();
-		Match para = regPara.Match(oriText);
-		Match paraClose = regParaClose.Match(oriText);            
-	//	subtitle.endOfLine = @"<br\s*/?>[\r\n]*";
-		if(para.Length>0 && paraClose.Length>0){
-		    string newText = oriText.Substring(para.Value.Length, oriText.Length-para.Value.Length -paraClose.Value.Length);
-	//	    subtitle.videoStart = para.Value;
-	//	    Subtitle.PossibleVideoStart = para.Value;
-	//	    subtitle.videoEnd = paraClose.Value;
-		    subtitle.Text = new SubtitleText(newText);
-		}
-	}
-	
-}
+    internal SubtitleFormatSAMI () {
+        name = "SAMI";
+        type = SubtitleType.SAMI;
+        mode = SubtitleMode.Times;
+        extensions = new string[] { "smi" };
+        lineBreak = "|";
 
+        format = @"\s*\<sami\>\s*([^<]|<(?!body>))*<body>";
+        headers=new string[2]{@"<SAMIParam[^>]*>([^<]|<(?!/SAMIParam>))*</SAMIParam>",@"<STYLE[^>]*>([^<]|<(?!/STYLE>))*</STYLE>" };
+        bodyEndIn = @"</BODY>\s*(</SAMI>)?"; //IGNORE CASE
+        bodyBeginOut = "<SAMI><HEAD></HEAD>\n<BODY>";
+        bodyEndOut = "</BODY>\n</SAMI>";
+
+        subtitleIn = @"<SYNC\s*start=[^0-9>]*(?<StartMilliseconds>\d*)[^0-9>]*>(?<Text>([^<]|<(?!/?SYNC))*)(</SYNC\s*>)?\s*<SYNC\s*start=[^0-9>]*(?<EndMilliseconds>\d*)[^0-9>]*>([^<]|<(?!/?SYNC))*(</SYNC\s*>)?";
+        subtitleOut = "<SYNC Start=\"<<StartTotalMilliseconds>>\"><P class=\"ENUSCC\"><<Text>></P></SYNC>\n<SYNC Start=\"<<EndTotalMilliseconds>>\"><P class=\"ENUSCC\">&#160;</P></SYNC>";
+    }
+
+    internal override void SubtitleInputPostProcess (Subtitle subtitle) {
+        Regex regPara = new Regex(@"\s*<P[^>]*>", RegexOptions.IgnoreCase);
+        Regex regParaClose = new Regex(@"</P\s*>\s*", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
+        string oriText = subtitle.Text.GetTrimLines();
+        Match para = regPara.Match(oriText);
+        Match paraClose = regParaClose.Match(oriText);
+    //    subtitle.endOfLine = @"<br\s*/?>[\r\n]*";
+        if(para.Length>0 && paraClose.Length>0){
+            string newText = oriText.Substring(para.Value.Length, oriText.Length-para.Value.Length -paraClose.Value.Length);
+    //        subtitle.videoStart = para.Value;
+    //        Subtitle.PossibleVideoStart = para.Value;
+    //        subtitle.videoEnd = paraClose.Value;
+            subtitle.Text = new SubtitleText(newText);
+        }
+    }
+}
 }
